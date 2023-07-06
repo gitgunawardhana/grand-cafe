deef66da3ef301e20b7822fef88904e8633bd667
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-07-07 04:32:29 +0530

    update landing navbar

--- client/src/components/LandingNavbar/index.tsx
+++ client/src/components/LandingNavbar/index.tsx
@@ -1,82 +1,77 @@
+import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
-import Logo100percentage from "../../assets/images/logo100percentage.svg";
 import { Button } from "../../base-components/Button";
-import Lucide from "../../base-components/Lucide";
+import Logo from "../../base-components/Logo";
+import LucideIcon from "../../base-components/LucideIcon";
+import { CSSClasses } from "../../constants";
+import getWindowSize from "../../utils/getWindowSize";
+import ProfileOptions from "../ProfileOptions";
 import { navigationLinks } from "./navigationLinks";
 
 function Main() {
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+  const [cls, setCls] = useState("");
+
+  useEffect(() => {
+    if (windowSize.width < 768) {
+      setCls("fixed right-[29px] top-[38px]");
+      console.log("Width   " + windowSize.width);
+    } else {
+      setCls("");
+    }
+  }, [windowSize.width]);
+
+  getWindowSize(setWindowSize);
+
   return (
     <div>
-      <nav className="fixed left-0 top-0 z-20 w-full border-b-2 border-[#ffe35299] !bg-transparent dark:border-[#ffe35299] dark:bg-gray-900">
+      <nav
+        className={twMerge(
+          "fixed left-0 top-0 z-20 w-full border-none !bg-transparent backdrop-blur-md dark:bg-gray-900"
+        )}
+      >
         <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
           <Button
             as="a"
             href="/"
             className="flex items-center !border-none !bg-transparent"
           >
-            <img
-              src={Logo100percentage}
-              className="mr-3 h-8"
-              alt="Flowbite Logo"
-            />
+            <Logo className="!h-14 !w-14" />
             <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
               GRAND CAFE
             </span>
           </Button>
-          <div className="flex md:order-2">
-            <Button
-              type="button"
-              className={twMerge([
-                "mr-3 rounded-lg !border-none !bg-transparent px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-[#ffe35299] dark:hover:bg-transparent dark:focus:ring-[#ffe35299] md:mr-0",
-                "focus:ring-none focus:ring-0 focus:ring-opacity-0",
-                "focus-visible:outline-none",
-                "dark:focus:ring-none dark:focus:ring-opacity-0",
-              ])}
-            >
-              <Lucide
-                icon="UserCircle"
-                className="mx-auto block text-[#ffe35299]"
-              />
+          <div className={twMerge(["flex md:order-2", cls])}>
+            <Button type="button" className={CSSClasses.NAVBARICON}>
+              <ProfileOptions />
             </Button>
-            <button
+            <Button
               data-collapse-toggle="navbar-sticky"
               type="button"
-              className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+              className="inline-flex h-9 w-9 translate-y-0.5 items-center justify-center rounded-full border-2 !border-gradient-yellow-500 !bg-transparent p-2 text-sm !text-gradient-yellow-500 hover:bg-transparent md:hidden"
               aria-controls="navbar-sticky"
               aria-expanded="false"
             >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="h-5 w-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
+              <span className="sr-only">Open</span>
+              <LucideIcon icon="MoreVertical" strokeWidth={2} />
+            </Button>
           </div>
           <div
             className="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
             id="navbar-sticky"
           >
-            <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 !bg-transparent bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
+            <ul className="mt-4 flex flex-col rounded-lg border border-gradient-yellow-500 !bg-transparent bg-gray-50 p-4 font-medium backdrop-blur-md md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0">
               {navigationLinks?.map((item, id) => (
                 <li key={id}>
                   <Button
                     as="a"
                     href={item.to}
-                    style={{
-                      color: `linear-gradient(180deg, rgba(255, 227, 83, 0.94) 0%, #FF9224 100%) !important`,
-                    }}
-                    className="block border-none !bg-transparent py-2 pl-3 pr-4 uppercase !text-[#ffe35299] text-gray-900 hover:bg-transparent md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                    className={twMerge([
+                      "block border-none !bg-transparent py-2 pl-3 pr-4 uppercase !text-gradient-yellow-500 hover:!text-gradient-yellow-900 md:p-0",
+                    ])}
                     aria-current="page"
                   >
                     {item.title}
