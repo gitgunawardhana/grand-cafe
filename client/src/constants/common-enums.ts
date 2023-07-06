c3d8a4f973c4c1eca10c772a076df1b0bfb7a477
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-07-07 04:34:05 +0530

    add enum file

---
+++ client/src/constants/common-enums.ts
@@ -0,0 +1,5 @@
+enum CSSClasses {
+  NAVBARICON = "m-0 rounded-lg !border-none !bg-transparent px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 dark:bg-[#ffe35299] dark:hover:bg-transparent md:mr-0",
+}
+
+export { CSSClasses };
