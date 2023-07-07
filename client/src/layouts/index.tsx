a08dcc2f4d1725d0c9c2dff4a128c973946ea42e
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-07-07 17:52:54 +0530

    Update index.tsx

--- client/src/layouts/index.tsx
+++ client/src/layouts/index.tsx
@@ -1,9 +1,21 @@
+import { twMerge } from "tailwind-merge";
+
 interface LayoutsProps {
   children: React.ReactElement | React.ReactElement[];
+  className?: string;
 }
 
 const Main = (props: LayoutsProps) => {
-  return <div className="mx-auto px-0 lg:px-0">{props.children}</div>;
+  return (
+    <div
+      className={twMerge([
+        "mx-auto !min-h-screen px-0 lg:px-0",
+        props.className,
+      ])}
+    >
+      {props.children}
+    </div>
+  );
 };
 
 export default Main;
