6c7fcfe318a79ef7d6b70e315fc3345026fd23a8
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-07-07 20:28:03 +0530

    Update index.tsx

--- client/src/base-components/Button/index.tsx
+++ client/src/base-components/Button/index.tsx
@@ -24,7 +24,8 @@ export const Button = <E extends React.ElementType = "a">({
 
   // General Styles
   const generalStyles = [
-    "max-w-fit transition duration-200 border shadow-sm inline-flex items-center justify-center py-3 px-4 rounded-md font-medium cursor-pointer inline-flex items-center justify-center text-base font-medium text-gray-500 bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white", // Default
+    "max-w-fit transition duration-200 border shadow-sm inline-flex items-center justify-center rounded-md font-medium cursor-pointer inline-flex items-center justify-center text-base font-medium text-gradient-yellow-300 bg-gradient-yellow-300 hover:text-gradient-yellow-300 hover:bg-gradient-yellow-300", // Default
+    "leading-normal shadow-[0_4px_9px_-5px] transition duration-150 ease-in-out hover:shadow-[0_4px_18px_0_rgba(96,255,83,0.2)]",
 
     "my-2",
   ];
