2c330bd7a6b875ce0ee3358a26921ba0b0247687
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-07-07 19:25:41 +0530

    Revert "Revert "fix dorpdown toggle""
    This reverts commit 2b5250a0aaaa9d9dd8ce415b3ad7d76f95db70d9.

--- client/src/components/LandingNavbar/index.tsx
+++ client/src/components/LandingNavbar/index.tsx
@@ -10,22 +10,32 @@ import ProfileOptions from "../ProfileOptions";
 import { navigationLinks } from "./navigationLinks";
 
 function Main() {
+  //* Set class name according to window width - start
   const [windowSize, setWindowSize] = useState({
     width: 0,
     height: 0,
   });
-  const [cls, setCls] = useState("");
+  const [clsProfileBtn, setClsProfileBtn] = useState("");
 
   useEffect(() => {
     if (windowSize.width < 768) {
-      setCls("fixed right-[29px] top-[38px]");
+      setClsProfileBtn("fixed right-[29px] top-[38px]");
     } else {
-      setCls("");
+      setClsProfileBtn("");
     }
   }, [windowSize.width]);
 
   getWindowSize(setWindowSize);
+  //* Set class name according to window width - end
 
+  //* Dropdown menu handler - start
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleToggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+    console.log("first " + isDropdownOpen);
+  };
+  //* Dropdown menu handler - end
   return (
     <div>
       <nav
@@ -44,7 +54,7 @@ function Main() {
               GRAND CAFE
             </span>
           </Button>
-          <div className={twMerge(["flex md:order-2", cls])}>
+          <div className={twMerge(["flex md:order-2", clsProfileBtn])}>
             <Button type="button" className={CSSClasses.NAVBARICON}>
               <ProfileOptions />
             </Button>
@@ -54,32 +64,35 @@ function Main() {
               className="inline-flex h-9 w-9 translate-y-0.5 items-center justify-center rounded-full border-2 !border-gradient-yellow-500 !bg-transparent p-2 text-sm !text-gradient-yellow-500 hover:bg-transparent md:hidden"
               aria-controls="navbar-sticky"
               aria-expanded="false"
+              onClick={handleToggleDropdown}
             >
               <span className="sr-only">Open</span>
               <LucideIcon icon="MoreVertical" strokeWidth={2} />
             </Button>
           </div>
-          <div
-            className="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
-            id="navbar-sticky"
-          >
-            <ul className="mt-4 flex flex-col rounded-lg border border-gradient-yellow-500 !bg-transparent bg-gray-50 p-4 font-medium backdrop-blur-md md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0">
-              {navigationLinks?.map((item, id) => (
-                <li key={id}>
-                  <Button
-                    as={NavLink}
-                    to={item.to}
-                    className={twMerge([
-                      "block border-none !bg-transparent py-2 pl-3 pr-4 uppercase !text-gradient-yellow-500 hover:!text-gradient-yellow-900 md:p-0",
-                    ])}
-                    aria-current="page"
-                  >
-                    {item.title}
-                  </Button>
-                </li>
-              ))}
-            </ul>
-          </div>
+          {isDropdownOpen && (
+            <div
+              className=" w-full items-center justify-between md:order-1 md:flex md:w-auto"
+              id="navbar-sticky"
+            >
+              <ul className="mt-4 flex flex-col rounded-lg border border-gradient-yellow-500 !bg-transparent bg-gray-50 p-4 font-medium backdrop-blur-md md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0">
+                {navigationLinks?.map((item, id) => (
+                  <li key={id}>
+                    <Button
+                      as={NavLink}
+                      to={item.to}
+                      className={twMerge([
+                        "block border-none !bg-transparent py-2 pl-3 pr-4 uppercase !text-gradient-yellow-500 hover:!text-gradient-yellow-900 md:p-0",
+                      ])}
+                      aria-current="page"
+                    >
+                      {item.title}
+                    </Button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </nav>
     </div>
