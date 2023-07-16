9a9c38dba9110d2fe5ef5668ea17380e76227cf3
Chamod Dilpa Gunarathna <73088073+Chamod17@users.noreply.github.com>
2023-07-16 18:10:40 +0530

    Responsiveness of first half

--- client/src/pages/HomePage/index.tsx
+++ client/src/pages/HomePage/index.tsx
@@ -9,79 +9,82 @@ import { Button } from "../../base-components/Button";
 import { NavLink } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
 
-
-
 const Main = () => {
   return (
     <div>
-        <div
-            style={{
-                backgroundImage: `url(${Back_01})`,
-                backgroundSize: "cover",
-          
-            }}
-            className="w-m flex flex-col p-5 pt-[80px] text-center text-slate-900  dark:text-slate-50 h-screen md:h-screen sm:h-screen"
-        >
-            <div className="grid grid-cols-3 p-5 items-center h-screen ">
-                <div className="flex justify-center items-center flex-col">
-                    <img src={Text_03} alt=""/>
-                    <br></br>
-                    <img src={Text_04} alt=""/>
-                    <br></br>
-                    <img src={Text_05} alt=""/>
-                    <br></br>
-                    <br></br>
-                    <div className="grid grid-cols-2">
-                        <div>
-                        <Button
-                    as={NavLink}
-                    to="/"
+      <div
+        style={{
+          backgroundImage: `url(${Back_01})`,
+          backgroundSize: "cover",
+        }}
+        className="w-m flex h-screen flex-col p-5 pt-[80px] text-center  text-slate-900 dark:text-slate-50 sm:h-screen md:h-screen"
+      >
+        <div className="grid h-screen items-center p-5 md:grid-cols-3 ">
+          <div className="flex flex-col items-center justify-center">
+            <img src={Text_03} alt="" />
+            <br></br>
+            <img src={Text_04} alt="" />
+            <br></br>
+            <img src={Text_05} alt="" />
+            <br></br>
+            <br></br>
+            <div className="flex flex-col sm:grid-cols-2 sm:flex-row">
+              <div>
+                <Button
+                  as={NavLink}
+                  to="/"
+                  className={twMerge(
+                    "rounded-[15px] border-2 border-solid border-amber-500 !bg-transparent px-[30px] py-[20px] lg:px-[45px] lg:py-[10.141px]"
+                  )}
+                >
+                  <span
                     className={twMerge(
-                      "rounded-[10px] border-2 border-solid border-gradient-yellow-300 !bg-transparent px-[20px] py-[10px] lg:px-[50px] lg:py-[10.141px]"
+                      "text-[14px] font-[900] uppercase tracking-[2px] !text-amber-500 hover:scale-110"
                     )}
                   >
-                    <span
-                      className={twMerge(
-                        "text-[14px] font-[900] uppercase tracking-[0px] !text-#FFE353 hover:scale-110"
-                      )}
-                    >
-                      Book A Table
-                    </span>
-                  </Button>
-                        </div>
-                        <div>
-                            
-                        </div>
-                   
-
-                    </div>
-                </div>
-                <div></div>
-                <div></div>
-                
+                    Book A Table
+                  </span>
+                </Button>
+              </div>
+              &nbsp;&nbsp;
+              <div>
+                <Button
+                  as={NavLink}
+                  to="/"
+                  className={twMerge(
+                    "rounded-[15px] border-2 border-solid border-gradient-yellow-300 !bg-amber-500 px-[20px] py-[20px] lg:px-[50px] lg:py-[10.141px]"
+                  )}
+                >
+                  <span
+                    className={twMerge(
+                      "!text-#FFE353 text-[14px] font-[900] uppercase tracking-[2px] hover:scale-110"
+                    )}
+                  >
+                    See Menu
+                  </span>
+                </Button>
+              </div>
             </div>
-            
-
+          </div>
+          <div></div>
+          <div></div>
         </div>
-        <div
-            style={{
-                backgroundImage: `url(${Back_02})`,
-                backgroundSize: "cover",
-          
-            }}
-            className="w-m flex flex-col p-5 pt-[80px] text-center text-slate-900  dark:text-slate-50 h-screen md:h-screen sm:h-screen"
-        >
-            <div className="flex justify-center items-center flex-col">
-                <img src={Text_01} alt=""/>
-                <br></br>
-                <img className="jsutify-center" src={Text_02} alt=""/>
-
-
-            </div>
+      </div>
+      <div
+        style={{
+          backgroundImage: `url(${Back_02})`,
+          backgroundSize: "cover",
+        }}
+        className="w-m flex h-screen flex-col p-5 pt-[80px] text-center  text-slate-900 dark:text-slate-50 sm:h-screen md:h-screen"
+      >
+        <div className="flex flex-col items-center justify-center">
+          <img src={Text_01} alt="" />
+          <br></br>
+          <img className="jsutify-center" src={Text_02} alt="" />
         </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
