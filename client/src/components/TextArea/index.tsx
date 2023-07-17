824d952f092f08aab045da26f56adc01c61373be
chathura <chathuradinushka97@gmail.com>
2023-07-17 17:30:34 +0530

    customer account

---
+++ client/src/components/TextArea/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+        <textarea className=' resize-none mt-3 bg-gradient-brown-900 border-gradient-yellow-900 rounded-lg' cols={30} rows={4}></textarea>
+    </div>
+  )
+}
+
+export default Main
--- client/src/pages/CustomerAccount/index.tsx
+++ client/src/pages/CustomerAccount/index.tsx
@@ -1,6 +1,8 @@
 import { Button } from "../../base-components/Button";
 import InputField from "../../base-components/FormElements/InputElement";
 import CheckBoxSetResponsive from "../../components/CheckBoxSetResponsive";
+import TextArea from "../../components/TextArea";
+import Image1 from "../../assets/images/CustomerAccount/user.png"
 
 const gender = [
   {
@@ -18,10 +20,18 @@ const gender = [
 
 const Main = () => {
   return (
-    <>  <div className="ml-[325px] mr-52 mt-0!m-auto flex flex-col justify-center object-cover px-12 text-center text-gradient-yellow-300 md:px-28 lg:px-32">
+    <>  
+    <div className="ml-[325px] mr-52 mt-0!m-auto flex flex-col justify-center object-cover px-12 text-center text-gradient-yellow-300 md:px-28 lg:px-32">
         
-        <div className="ml-[25px] col-span-2 mt-5 justify-start text-start text-[20px] font-extrabold text-gradient-yellow-900 sm:text-[25px] md:text-[30px] lg:text-[45px]">
+        <div className="ml-[25px] col-span-2 mt-5 justify-start text-start text-[20px] font-black text-gradient-yellow-900 sm:text-[20px] md:text-[30px] lg:text-[40px]">
               <h1>Account Settings</h1>
+        </div>
+
+        <div className="flex flex-2">
+          <div className=""><img 
+          className="absolute ml-[25px] mt-9 w-[154px] h-[154px] object-cover" src={Image1}></img>
+          </div>
+          
         </div>
         <div className="mb-16 mt-20 grid grid-cols-2  items-center justify-start gap-1 md:mt-40 lg:mt-40 lg:flex-row">
         
@@ -58,6 +68,7 @@ const Main = () => {
         <div className="mt-[70px] ml-20  col-span-2 justify-start text-start sm:col-span-1">
           <label className="pb-[20px]  text-gradient-yellow-900">Gender </label>
               <CheckBoxSetResponsive
+                className="mt-2 pl-5"
                 dataset={gender}
                 type="radio"
                 name="gender"
@@ -72,8 +83,13 @@ const Main = () => {
               />
         </div>
         <div className="mt-10 ml-20 text-start">
-        <label className="pb-[20px] my-10 text-gradient-yellow-900">Redential Address </label>
-        <textarea  rows={5} cols={20} className=" rounded-md block p-2.5 w-full text-sm opacity-0 border border-gradient-yellow-900"></textarea>
+          <label className="pb-[20px] my-10 text-gradient-yellow-900">Redential Address </label>
+            <TextArea/>
+        </div>
+
+        <div>
+          <Button className="bg-gradient-to-b from-yellow-500 to-yellow-300 hover:bg-gradient-yellow-900 hover:text-none justify-items-start border !border-gradient-yellow-900 text-sm mt-28 ml-0 px-4 py-3 text-black ">Save Changes</Button>
+          
         </div>
             </div>
             </div>
