fee4bb81551750289554e085be8aac92ba047df1
chathura <chathuradinushka97@gmail.com>
2023-08-14 23:39:52 +0530

    add kommunicate plugin

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+Kommunicate.init("30b4dfef7097d37448b61620dc8428762", {
+  automaticChatOpenOnNavigation: true,
+  popupWidget: true
+});
