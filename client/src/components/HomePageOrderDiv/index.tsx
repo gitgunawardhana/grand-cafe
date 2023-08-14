07cba9be90efdf975fe108d8373ec5012d361b77
Chamod Dilpa Gunarathna <73088073+Chamod17@users.noreply.github.com>
2023-08-14 15:12:56 +0530

    Image Gallery animated

--- client/src/components/HomePageOrderDiv/index.tsx
+++ client/src/components/HomePageOrderDiv/index.tsx
@@ -1,27 +1,81 @@
-
+import React, { useEffect, useRef, useState } from 'react';
 
 interface CardProps {
-    imageSrc: string;
-    title: string;
-    description: string;
-    buttonText: string;
-  }
+  imageSrc: string;
+  title: string;
+  description: string;
+  buttonText: string;
+}
 
-const Main: React.FC<CardProps> =({ imageSrc, title, description, buttonText }) => {
+const Main: React.FC<CardProps> = ({ imageSrc, title, description, buttonText }) => {
+  const mainRef = useRef<HTMLDivElement | null>(null);
+  const [shouldAnimate, setShouldAnimate] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        setShouldAnimate(entry.isIntersecting);
+      });
+    }, { threshold: 0.5 });
+
+    if (mainRef.current) {
+      observer.observe(mainRef.current);
+    }
+
+    return () => {
+      if (mainRef.current) {
+        observer.unobserve(mainRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className="  rounded-lg shadow-lg p-4">
+    <div ref={mainRef} className={`rounded-lg shadow-lg p-4 ${shouldAnimate ? 'appear-animation' : ''}`}>
       <div className="flex justify-center items-center">
         <img src={imageSrc} alt="Card" className=" " />
       </div>
       <div className="p-4">
         <h3 className="text-xl font-semibold mb-2 text-amber-500">{title}</h3>
-        <p className="text-white text-md p-5  mb-4">{description}</p><br></br>
-        <button className=" rounded-[15px] border-2 border-solid border-amber-500 !bg-transparent px-[30px] py-[20px] lg:px-[45px] lg:py-[10.141px] hover:scale-110 ">
+        <p className="text-white text-md p-5 mb-4">{description}</p><br></br>
+        <button className="rounded-[15px] border-2 border-solid border-amber-500 !bg-transparent px-[30px] py-[20px] lg:px-[45px] lg:py-[10.141px] hover:scale-110">
           {buttonText}
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
+
+
+
+
+
+
+
+
+// interface CardProps {
+//     imageSrc: string;
+//     title: string;
+//     description: string;
+//     buttonText: string;
+//   }
+
+// const Main: React.FC<CardProps> =({ imageSrc, title, description, buttonText }) => {
+//   return (
+//     <div className="  rounded-lg shadow-lg p-4">
+//       <div className="flex justify-center items-center">
+//         <img src={imageSrc} alt="Card" className=" " />
+//       </div>
+//       <div className="p-4">
+//         <h3 className="text-xl font-semibold mb-2 text-amber-500">{title}</h3>
+//         <p className="text-white text-md p-5  mb-4">{description}</p><br></br>
+//         <button className=" rounded-[15px] border-2 border-solid border-amber-500 !bg-transparent px-[30px] py-[20px] lg:px-[45px] lg:py-[10.141px] hover:scale-110 ">
+//           {buttonText}
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Main
--- client/src/components/ImageGallery/index.tsx
+++ client/src/components/ImageGallery/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Grid1 from "../../assets/images/HomePage/Grid (1).png";
 import Grid2 from "../../assets/images/HomePage/Grid (2).png";
 import Grid3 from "../../assets/images/HomePage/Grid (3).png";
@@ -8,60 +9,58 @@ import Grid6 from "../../assets/images/HomePage/Grid (6).png";
 const index = () => {
   return (
     <div>
-       <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
-  <div className="h-full flex flex-wrap md:-m-2">
-    <div className=" h-full flex w-1/2 flex-wrap">
-      <div className="w-1/2 p-1 md:p-2">
-        <img
-          alt="gallery"
-          className="block h-full w-full rounded-lg object-cover object-center"
-          src={Grid1}
-        />
-      </div>
-      <div className="w-1/2 p-1 md:p-2">
-        <img
-          alt="gallery"
-          className="block h-full w-full rounded-lg object-cover object-center"
-          src={Grid2}
-        />
-      </div>
-      <div className="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          className="block h-full w-full rounded-lg object-cover object-center"
-          src={Grid3}
-        />
+      <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
+        <div className="h-full flex flex-wrap md:-m-2">
+          <div className=" h-full flex w-1/2 flex-wrap">
+            <div className="w-1/2 p-1 md:p-2 image-container">
+              <img
+                alt="gallery"
+                className="block h-full w-full rounded-lg object-cover object-center image"
+                src={Grid1}
+              />
+            </div>
+            <div className="w-1/2 p-1 md:p-2 image-container">
+              <img
+                alt="gallery"
+                className="block h-full w-full rounded-lg object-cover object-center image"
+                src={Grid2}
+              />
+            </div>
+            <div className="w-full p-1 md:p-2 image-container">
+              <img
+                alt="gallery"
+                className="block h-full w-full rounded-lg object-cover object-center image"
+                src={Grid3}
+              />
+            </div>
+          </div>
+          <div className="flex w-1/2 flex-wrap">
+            <div className="w-full p-1 md:p-2 image-container">
+              <img
+                alt="gallery"
+                className="block h-full w-full rounded-lg object-cover object-center image"
+                src={Grid4}
+              />
+            </div>
+            <div className="w-1/2 p-1 md:p-2 image-container">
+              <img
+                alt="gallery"
+                className="block h-full w-full rounded-lg object-cover object-center image"
+                src={Grid5}
+              />
+            </div>
+            <div className="w-1/2 p-1 md:p-2 image-container">
+              <img
+                alt="gallery"
+                className="block h-full w-full rounded-lg object-cover object-center image"
+                src={Grid6}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div className="flex w-1/2 flex-wrap">
-      <div className="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          className="block h-full w-full rounded-lg object-cover object-center"
-          src={Grid4}
-        />
-      </div>
-      <div className="w-1/2 p-1 md:p-2">
-        <img
-          alt="gallery"
-          className="block h-full w-full rounded-lg object-cover object-center"
-          src={Grid5}
-        />
-      </div>
-      <div className="w-1/2 p-1 md:p-2">
-        <img
-          alt="gallery"
-          className="block h-full w-full rounded-lg object-cover object-center"
-          src={Grid6}
-        />
-      </div>
-    </div>
-  </div>
-</div>
+  );
+};
 
-      
-    </div>
-  )
-}
-
-export default index
+export default index;
