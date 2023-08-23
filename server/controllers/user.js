08a0ee226d51bf3886df9ea74aa59a921ffa25e5
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-08-23 10:56:28 +0530

    create seat booking endpoint

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,5 @@
 import Seat from "../models/Seat.js";
+import SeatBooking from "../models/SeatBooking.js";
 
 export const getAllSeats = async (req, res) => {
   try {
@@ -13,13 +14,10 @@ export const updateSeats = async (req, res) => {
   try {
     const updatedSeats = req.body.updatedSeats;
 
-    // Loop through the updated tasks and update the database
+    // Loop through the updated seats and update the database
     for (const updatedSeat of updatedSeats) {
       const number = updatedSeat.number;
 
-      console.log("number ", number);
-
-      // Update the task in the database
       const updatedSeatNew = await Seat.findOneAndUpdate(
         { number: number },
         { number: updatedSeat.number, available: updatedSeat.available },
@@ -31,6 +29,21 @@ export const updateSeats = async (req, res) => {
     }
 
     return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+export const seatBooking = async (req, res) => {
+  const newSeatBooking = new SeatBooking({
+    user: req.user.id,
+    dateTime: req.body.dateTime,
+    bookingSeats: req.body.bookingSeats,
+  });
+
+  try {
+    const savedSeatBooking = await newSeatBooking.save();
+    res.status(200).json(savedSeatBooking);
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getAllSeats, updateSeats } from "../controllers/user.js";
+import { getAllSeats, seatBooking, updateSeats } from "../controllers/user.js";
+import { verify } from "../utils/checkAuth.js";
 
 const router = express.Router();
 
 router.get("/get-all-seats", getAllSeats);
 router.put("/update-all-seats", updateSeats);
+router.post("/seat-booking", verify, seatBooking);
 
 export default router;
