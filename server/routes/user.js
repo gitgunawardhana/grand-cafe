9d7d90bd3debd19e2cce2660f5784142cc6e47f8
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-08-31 23:41:18 +0530

    create seat booking endpoints

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,23 @@
 import express from "express";
-import { getAllSeats, seatBooking, updateSeats } from "../controllers/user.js";
+import {
+  createSeatBooking,
+  deleteSeatBookingById,
+  deleteSeatBookingByUser,
+  getAllSeatBooking,
+  getAllSeats,
+  getBookingSeatsByUserId,
+  updateSeats,
+} from "../controllers/user.js";
 import { verify } from "../utils/checkAuth.js";
 
 const router = express.Router();
 
-router.get("/get-all-seats", getAllSeats);
-router.put("/update-all-seats", updateSeats);
-router.post("/seat-booking", verify, seatBooking);
+router.get("/get-all-seats", verify, getAllSeats);
+router.put("/update-all-seats", verify, updateSeats);
+router.post("/create-seat-booking", verify, createSeatBooking);
+router.get("/get-all-seat-booking", verify, getAllSeatBooking);
+router.get("/get-all-seat-booking-by-user", verify, getBookingSeatsByUserId);
+router.delete("/delete-seats-booking/:id", verify, deleteSeatBookingById);
+router.delete("/delete-seats-booking-by-user", verify, deleteSeatBookingByUser);
 
 export default router;
