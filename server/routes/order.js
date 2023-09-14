cce679d26f7361def7b20dfe619f275c16cbf84a
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-09-14 16:47:19 +0530

    add revenue data

--- server/routes/order.js
+++ server/routes/order.js
@@ -4,6 +4,7 @@ import {
   getOrdersCountByCurrentMonth,
   getOrdersCountByMonth,
   getOrdersForCurrentMonth,
+  getRevenueByMonth,
   viewOrder,
 } from "../controllers/order.js";
 const router = express.Router();
@@ -14,5 +15,6 @@ router.use("/addOrder", addOrder);
 router.get("/current-month", getOrdersForCurrentMonth);
 router.get("/count-by-current-month", getOrdersCountByCurrentMonth);
 router.get("/count-by-month", getOrdersCountByMonth);
+router.get("/revenue-by-month", getRevenueByMonth);
 
 export default router;
